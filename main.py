ba3d0af377b6cdd32c588c6af0441d65470db697
jishnu <jishnusurajila@gmail.com>
2020-06-02 14:16:57 +0530

    changes in fit method

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 import numpy
 
 class LinearRegression(object):
-  """
-  An implementation of linear regression model
-  """
+  
   
   def fit(_input, _output):
-    pass
+    x=_input
+    y=_output
+    x_mean,y_mean=numpy.mean(x),numpy.mean(y)
+    b1=numpy.cov(x,x_mean,y,y_mean)/numpy.var(x,x_mean)
+    b0=y_mean-b1*x_mean
+    
+
+    
     
   def predict(_input):
     pass
