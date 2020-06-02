9f5c21ddc6a2516691d87ae39e4f0a3a6d451163
jishnu <jishnusurajila@gmail.com>
2020-06-02 16:55:31 +0530

    function complete

--- main.py
+++ main.py
@@ -6,31 +6,36 @@ x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
 """
 
 import numpy as np
-
+b0=0.0
+b1=0.0
 class LinearRegression(object):
+
 #function for variance
-  def variance(values,mean):
+  def variance(self,values,mean):
     return sum([(x-mean)**2 for x in values])
 #function for covariance
-  def covariance(x,mean_x,y,mean_y):
+  def covariance(self,x,mean_x,y,mean_y):
     covar=0
     for i in range(len(x)):
       covar+=((x[i]-mean_x)*(y[i]-mean_y))
       return covar
 
-
   def fit(self,_input, _output):
+    global b1,b0
     x=_input
     y=_output
     x_mean,y_mean=np.mean(x),np.mean(y)
-    b1=covariance(x,x_mean,y,y_mean)/variance(x,x_mean)
-    b0=y_mean-b1*x_mean
-    return[b0,b1]
+    b1 =self.covariance(x,x_mean,y,y_mean)/self.variance(x,x_mean)
+    b0 =y_mean-b1*x_mean
+    print (b0,b1)
+    
 
     
     
-  def predict(_input):
-    b0,b1=
+  def predict(self,_input):
+    global b0,b1
+    y=b0+b1*_input
+    return y
     pass
     
     
