99a9b131721a594cb8f0927a577391e5d9ac0d1a
jishnu <jishnusurajila@gmail.com>
2020-06-02 20:48:11 +0530

    format correction

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-"""
-Implement the linear regression model using python and numpy in the following class.
-The method fit() should take inputs like,
-x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
-
-"""
-
 import numpy as np
 b0=0.0
 b1=0.0
@@ -28,9 +21,6 @@ class LinearRegression(object):
     b1 =self.covariance(x,x_mean,y,y_mean)/self.variance(x,x_mean)
     b0 =y_mean-b1*x_mean
     print (b0,b1)
-    
-
-    
     
   def predict(self,_input):
     global b0,b1
